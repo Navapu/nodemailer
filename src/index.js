700f1017b6cbf42c6cd82dd8a227abd33c08d9df
Navapu <drojannp2@gmail.com>
2023-02-21 02:52:09 +0100

    Creo el formulario pero aun no es funcional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const indexRouter = require('./routes/index');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use('/', indexRouter)
+app.listen(3000)
+console.log("Escuchando en el puerto 3000")
